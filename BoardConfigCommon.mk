056ace1c9657a95298fafdc03b734230e54f94c2
Leonardo Meitz <leonardomeitz@gmail.com>
2020-07-27 14:45:17 +0200

    adb fix and server other fixes...

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -45,6 +45,8 @@ BOARD_KERNEL_PAGESIZE    := 2048
 BOARD_KERNEL_TAGS_OFFSET := 0x00000100
 BOARD_RAMDISK_OFFSET     := 0x01000000
 TARGET_KERNEL_SOURCE := kernel/tcl/msm8952
+#Needed in order to avoid clang issues
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
 
 # ANT
 BOARD_ANT_WIRELESS_DEVICE := "vfs-prerelease"
